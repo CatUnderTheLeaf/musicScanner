15ee45d904ae4e80f67cb6612f88eed6cb430382
CatUnderTheLeaf <catundertheleaf@gmail.com>
2024-11-08 17:47:13 +0100

    update app

--- musicScannerApp.py
+++ musicScannerApp.py
@@ -1,24 +1,34 @@
 import streamlit as st
-import numpy as np
-
-import cv2
 
 import scanner.detection as detector
+import scanner.utils as utils
+
+# make a wide layout, not with a fixed width in the center 
+st.set_page_config(
+    page_title="Music sheet scanner",
+    layout="wide",
+)
 
 st.title('Scan your music')
 
-def get_photo(img_file, key):
-    bytes_data = img_file.getvalue()
-    cv2_img = cv2.imdecode(np.frombuffer(bytes_data, np.uint8), cv2.IMREAD_COLOR)
-    notes_image = cv2.cvtColor(cv2_img, cv2.COLOR_BGR2RGB)
-    det_im = detector.detect_everything(notes_image, hide_labels=False, rect_th=2, text_size=1)
-    st.image(det_im)
-
 def main():
-    image = st.file_uploader(label_visibility = "collapsed", type=['png', 'jpg'], label='upload picture')
-    if image:
-        get_photo(image, 'upload')
+    uploaded_image = st.file_uploader(label_visibility = "collapsed", type=['png', 'jpg', 'jpeg','pdf'], label='upload picture') #, accept_multiple_files=True
+    
+    decoded_image = utils.load_test_image()
+    caption = 'test_image'
+    if uploaded_image:
+        decoded_image = utils.get_photo(uploaded_image, 'upload')
+        caption = ''
+    
+    detections = detector.detect_everything(decoded_image)
+    classes_to_view = set(['staff'])
 
+    sliced_staffs = detector.slice_image(detections, divider='staff')
+    for staff in sliced_staffs:
+        visual_staff = detector.visualize_predictions(staff['image'], staff['predictions'], filter=classes_to_view, hide_labels=False, rect_th=2, text_size=1)
+        st.image(visual_staff, caption)
+        st.divider()
+        
 
 if __name__ == "__main__":
     main()
